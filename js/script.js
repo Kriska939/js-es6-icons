248312e7d82c6756bd3167b7827e10e48b9f15f6
Kriska939 <cristinacorrado93@gmail.com>
2021-07-26 21:54:50 +0200

    added milestone 1

--- js/script.js
+++ js/script.js
@@ -10,3 +10,32 @@ Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
 */
 
+// {
+//     name: 'cat',
+//     prefix: 'fa-',
+//     type: 'animal',
+//     family: 'fas',
+//   }
+
+// MILESTONE 1:
+
+let cardTemplate = "";
+
+icons.forEach((icon) => {
+    cardTemplate += `
+    <div class="col col-sm-4 col-md-2">
+        <div class="card">
+            <div class="card-body text-center">
+                <i class="${icon.family} ${icon.prefix}${icon.name} ${icon.prefix}2x"></i>
+                    <p>${icon.name.toLocaleUpperCase()}</p>
+            </div>
+        </div>
+    </div>`;
+});
+
+const displayHere = document.querySelector("#container-icons .row");
+
+displayHere.innerHTML = cardTemplate;
+
+//---------------------Milestone 2: 
+
